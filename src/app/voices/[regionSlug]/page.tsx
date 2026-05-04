914220391ef7f8ef2202d368b225da8a27d0979f
ben ross <benjamin.nexia@gmail.com>
2026-05-04 05:51:10 -0600

    emb wk 4 pdcast

--- src/app/voices/[regionSlug]/page.tsx
+++ src/app/voices/[regionSlug]/page.tsx
@@ -498,13 +498,13 @@ export default async function VoicesRegionPage({ params }: Props) {
             </h3>
             <iframe
               className="mt-[15px]"
-              title="Finding the Feeling of Home"
+              title="#4 When Stories of Place Come to Life"
               height="150"
               width="100%"
               style={{ border: "none", minWidth: "min(100%, 430px)", height: "150px" }}
               scrolling="no"
               data-name="pb-iframe-player"
-              src="https://www.podbean.com/player-v2/?i=kwg9j-1a9e8f2-pb&from=pb6admin&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&btn-skin=7"
+              src="https://www.podbean.com/player-v2/?i=viygc-1aa5dad-pb&from=pb6admin&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&btn-skin=7"
               loading="lazy"
             />
           </div>
